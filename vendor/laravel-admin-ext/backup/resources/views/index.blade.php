fe7adc15ea2b433288ec63c515dc8abce10703c9
kamal <kamaludinakbar0@gmail.com>
2021-03-03 16:23:59 +0700

    add alternative backup

--- vendor/laravel-admin-ext/backup/resources/views/index.blade.php
+++ vendor/laravel-admin-ext/backup/resources/views/index.blade.php
@@ -1,62 +1,62 @@
 <script data-exec-on-popstate>
-$(function () {
+    $(function() {
 
-    $(".backup-run").click(function() {
-        var $btn = $(this);
-        $btn.button('loading');
+        $(".backup-run").click(function() {
+            var $btn = $(this);
+            $btn.button('loading');
 
-        NProgress.start();
-        $.ajax({
-            url: $btn.attr('href'),
-            data : {
-                _token: LA.token
-            },
-            method: 'POST',
-            success: function (data){
+            NProgress.start();
+            $.ajax({
+                url: $btn.attr('href'),
+                data: {
+                    _token: LA.token
+                },
+                method: 'POST',
+                success: function(data) {
 
-                if (data.status) {
-                    $('.output-box').removeClass('hide');
-                    $('.output-box .output-body').html(data.message)
-                }
-
-                $btn.button('reset');
-                NProgress.done();
-            }
-        });
-
-        return false;
-    });
-
-    $(".backup-delete").click(function() {
-
-        var $btn = $(this);
-
-        $.ajax({
-            url: $btn.attr('href'),
-            data : {
-                _token: LA.token
-            },
-            method: 'DELETE',
-            success: function (data){
-
-                $.pjax.reload('#pjax-container');
-
-                if (typeof data === 'object') {
                     if (data.status) {
-                        toastr.success(data.message);
-                    } else {
-                        toastr.error(data.message);
+                        $('.output-box').removeClass('hide');
+                        $('.output-box .output-body').html(data.message)
                     }
-                }
 
-                $btn.button('reset');
-            }
+                    $btn.button('reset');
+                    NProgress.done();
+                }
+            });
+
+            return false;
         });
 
-        return false;
-    });
+        $(".backup-delete").click(function() {
 
-});
+            var $btn = $(this);
+
+            $.ajax({
+                url: $btn.attr('href'),
+                data: {
+                    _token: LA.token
+                },
+                method: 'DELETE',
+                success: function(data) {
+
+                    $.pjax.reload('#pjax-container');
+
+                    if (typeof data === 'object') {
+                        if (data.status) {
+                            toastr.success(data.message);
+                        } else {
+                            toastr.error(data.message);
+                        }
+                    }
+
+                    $btn.button('reset');
+                }
+            });
+
+            return false;
+        });
+
+    });
 </script>
 
 <style>
@@ -74,7 +74,6 @@ $(function () {
         color: #3c8dbc;
         margin-left: 10px;
     }
-
 </style>
 
 <div class="box">
@@ -89,45 +88,45 @@ $(function () {
     <div class="box-body table-responsive no-padding">
         <table class="table table-hover">
             <tbody>
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Disk</th>
-                <th>Reachable</th>
-                <th>Healthy</th>
-                <th># of backups</th>
-                <th>Newest backup</th>
-                <th>Used storage</th>
-            </tr>
-            @foreach($backups as $index => $backup)
-            <tr data-toggle="collapse" data-target="#trace-{{$index+1}}" style="cursor: pointer;">
-                <td>{{ $index+1 }}.</td>
-                <td>{{ @$backup[0] }}</td>
-                <td>{{ @$backup['disk'] }}</td>
-                <td>{{ @$backup[1] }}</td>
-                <td>{{ @$backup[2] }}</td>
-                <td>{{ @$backup['amount'] }}</td>
-                <td>{{ @$backup['newest'] }}</td>
-                <td>{{ @$backup['usedStorage'] }}</td>
-            </tr>
-            <tr class="collapse" id="trace-{{$index+1}}">
-                <td colspan="8">
-                    <ul class="todo-list ui-sortable">
-                        @foreach($backup['files'] as $file)
-                        <li>
-                            <span class="text">{{ $file }}</span>
-                            <!-- Emphasis label -->
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Disk</th>
+                    <th>Reachable</th>
+                    <th>Healthy</th>
+                    <th># of backups</th>
+                    <th>Newest backup</th>
+                    <th>Used storage</th>
+                </tr>
+                @foreach($backups as $index => $backup)
+                <tr data-toggle="collapse" data-target="#trace-{{$index+1}}" style="cursor: pointer;">
+                    <td>{{ $index+1 }}.</td>
+                    <td>{{ @$backup[0] }}</td>
+                    <td>{{ @$backup['disk'] }}</td>
+                    <td>{{ @$backup[1] }}</td>
+                    <td>{{ @$backup[2] }}</td>
+                    <td>{{ @$backup['amount'] }}</td>
+                    <td>{{ @$backup['newest'] }}</td>
+                    <td>{{ @$backup['usedStorage'] }}</td>
+                </tr>
+                <tr class="collapse" id="trace-{{$index+1}}">
+                    <td colspan="8">
+                        <ul class="todo-list ui-sortable">
+                            @foreach($backup['files'] as $file)
+                            <li>
+                                <span class="text">{{ $file }}</span>
+                                <!-- Emphasis label -->
 
-                            <div class="tools">
-                                <a target="_blank" href="{{ route('backup-download', ['disk' => $backup['disk'], 'file' => $backup[0].'/'.$file]) }}"><i class="fa fa-download"></i></a>
-                                <a href="{{ route('backup-delete', ['disk' => $backup['disk'], 'file' => $backup[0].'/'.$file]) }}" class="backup-delete"><i class="fa fa-trash-o"></i></a>
-                            </div>
-                        </li>
-                        @endforeach
-                    </ul>
-                </td>
-            </tr>
-            @endforeach
+                                <div class="tools">
+                                    <a target="_blank" href="{{ route('backup-download', ['disk' => $backup['disk'], 'file' => $backup[0].'/'.$file]) }}"><i class="fa fa-download"></i></a>
+                                    <a href="{{ route('backup-delete', ['disk' => $backup['disk'], 'file' => $backup[0].'/'.$file]) }}" class="backup-delete"><i class="fa fa-trash-o"></i></a>
+                                </div>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </td>
+                </tr>
+                @endforeach
 
             </tbody>
         </table>
--- vendor/laravel-admin-ext/backup/src/BackupController.php
+++ vendor/laravel-admin-ext/backup/src/BackupController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class BackupController
 {
@@ -16,15 +17,25 @@ class BackupController
      *
      * @return Content
      */
+    public function remove_tag($string, $tagname)
+    {
+        $pattern = "#<\s*?$tagname\b[^>]*>(.*?)</$tagname\b[^>]*>#s";
+        preg_match($pattern, $string, $matches);
+        return isset($matches[1]) ? $matches[1] : $string;
+    }
+
     public function index()
     {
         return Admin::content(function (Content $content) {
             $content->header('Backup');
 
-            $backup = new Backup();
-
-            $content->body(view('laravel-admin-backup::index', [
-                'backups' => $backup->getExists(),
+           $tables = $this->allTablesMySQL();
+           dd($tables);
+           $backup = new Backup();
+           $x = $backup->getExists();
+           $x[0]['disk'] = $this->remove_tag($x[0]['disk'], 'error');
+           $content->body(view('laravel-admin-backup::index', [
+                'backups' => $x,
             ]));
         });
     }
@@ -57,27 +68,6 @@ class BackupController
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function run()
-    {
-        try {
-            ini_set('max_execution_time', 300);
-
-            // start the backup process
-            Artisan::call('backup:run --only-db --disable-notifications');
-
-            $output = Artisan::output();
-
-            return response()->json([
-                'status'  => true,
-                'message' => $output,
-            ]);
-        } catch (\Exception $e) {
-            return response()->json([
-                'status'  => false,
-                'message' => $e->getMessage(),
-            ]);
-        }
-    }
 
     /**
      * Delete a backup file.
@@ -104,5 +94,80 @@ class BackupController
             'status'  => false,
             'message' => trans('admin.delete_failed'),
         ]);
+    }
+    public static function allTablesMySQL($connection = null)
+    {
+        return collect(DB::connection($connection)->select('show tables'))->map(function ($val) {
+            foreach ($val as $key => $tbl) {
+                return $tbl;
+            }
+        });
+    }
+    public function run () {
+        $get_all_table_query = "SHOW TABLES";
+        $result = DB::select(DB::raw($get_all_table_query));
+        $tables = $this->allTablesMySQL();
+
+        $structure = '';
+        $data = '';
+        foreach ($tables as $table) {
+            $show_table_query = "SHOW CREATE TABLE " . $table . "";
+
+            $show_table_result = DB::select(DB::raw($show_table_query));
+
+            foreach ($show_table_result as $show_table_row) {
+                $show_table_row = (array)$show_table_row;
+                $structure .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
+            }
+            $select_query = "SELECT * FROM " . $table;
+            $records = DB::select(DB::raw($select_query));
+
+            foreach ($records as $record) {
+                $record = (array)$record;
+                $table_column_array = array_keys($record);
+                foreach ($table_column_array as $key => $name) {
+                    $table_column_array[$key] = '`' . $table_column_array[$key] . '`';
+                }
+
+                $table_value_array = array_values($record);
+                $data .= "\nINSERT INTO $table (";
+
+                $data .= "" . implode(", ", $table_column_array) . ") VALUES \n";
+
+                foreach ($table_value_array as $key => $record_column)
+                    $table_value_array[$key] = addslashes($record_column);
+
+                $data .= "('" . implode("','", $table_value_array) . "');\n";
+            }
+        }
+        $file_name = storage_path('app/Laravel/'). date('Y-m-d-h-i-s') . '.sql';
+        $file_handle = fopen($file_name, 'w + ');
+
+        $output = $structure . $data;
+        fwrite($file_handle, $output);
+        fclose($file_handle);
+        echo "DB backup ready";
+    }
+
+    public function run2()
+    {
+        try {
+            ini_set('max_execution_time', 300);
+
+            // start the backup process
+            Artisan::call('backup:run --only-db --disable-notifications');
+
+            $output = Artisan::output();
+
+            return response()->json([
+                'status'  => true,
+                'message' => $output,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status'  => false,
+                'message' => $e->getMessage(),
+            ]);
+        }
     }
 }
