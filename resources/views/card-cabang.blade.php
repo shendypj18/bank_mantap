dfecce5285f45fa8ef73d06ea2e8e3594c62a67b
kamal <kamaludinakbar0@gmail.com>
2021-04-09 15:30:38 +0700

    update bug map

--- resources/views/card-cabang.blade.php
+++ resources/views/card-cabang.blade.php
@@ -9,7 +9,7 @@
                 <img src="{{ asset('asset/logo_mantap.png') }}" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
                 <small>{{$c->alamat}}</small><br/>
                 <small>{{$c->telp}}</small></p>
-                <h3><img src="{{ asset('asset/icon/maps.png') }}"  alt="Maps"  style="width: 5%"> &nbsp;<a href="">Lihat Map</a></h3>
+                <h3><img src="{{ asset('asset/icon/maps.png') }}"  alt="Maps"  style="width: 5%"> &nbsp;<a>Lihat Map</a></h3>
             </div>
         </div>
     </div>
