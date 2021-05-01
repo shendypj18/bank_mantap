8984043297ed404382d0a768ea414300962d638f
kamal <kamaludinakbar0@gmail.com>
2021-05-01 18:58:27 +0700

    update nomer 63

--- public/js/x.js
+++ public/js/x.js
@@ -3,13 +3,13 @@
     //document.getElementsByClassName('fileinput-remove').disabled = "";
 
 //});
-$( document ).ready(function() {
-    //$(".fileinput-remove").prop('disabled', true);
-    //$(".fileinput-remove").hide();
-    // $(':reset').click(function () {
-    //     location.reload(true);
-    //     //location.reload();
-    //     //javascript:void(0);
-    //     //return false;
-    // });
-});
+// $( document ).ready(function() {
+//     //$(".fileinput-remove").prop('disabled', true);
+//     //$(".fileinput-remove").hide();
+//     // $(':reset').click(function () {
+//     //     location.reload(true);
+//     //     //location.reload();
+//     //     //javascript:void(0);
+//     //     //return false;
+//     // });
+// });
