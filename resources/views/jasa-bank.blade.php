18b50a6246a84e3283b9e4724d9f6e374750d35b
kamal <kamaludinakbar0@gmail.com>
2021-05-24 02:39:22 +0700

    update jasa-bank

--- resources/views/jasa-bank.blade.php
+++ resources/views/jasa-bank.blade.php
@@ -1,90 +1,173 @@
+
+<style type="text/css" media="screen">
+
+ @media (min-width: 1025px) {
+     .btn-pos {
+         margin-top: -250px;
+     }
+ }
+
+ .right-text{
+     margin-left: 90px;
+ }
+ .right-img {
+     margin-right: 5px;
+ }
+ .btn-right {
+     margin-left: -450px;
+ }
+ .r-btn {
+     margin-top: -170px;
+     float:right;
+ }
+.mc {
+     margin-bottom: 150px;
+ }
+ @media (max-width: 768px) {
+     .mc {
+         margin-bottom: 0px;
+     }
+     .right-text{
+         margin-left: 0px;
+     }
+     .btn-right {
+         margin-left: 0px;
+     }
+     .r-btn {
+         margin-top: 0px;
+         float:left;
+     }
+     .btn-pos {
+         margin-top: -7%;
+     }
+     .mc-r  {
+         display: -webkit-box;
+         display: -moz-box;
+         display: -ms-flexbox;
+         display: -webkit-flex;
+         display: flex;
+         -webkit-box-orient: vertical;
+         -moz-box-orient: vertical;
+         -webkit-flex-direction: column;
+         -ms-flex-direction: column;
+         flex-direction: column;
+         /* optional */
+         -webkit-box-align: start;
+         -moz-box-align: start;
+         -ms-flex-align: start;
+         -webkit-align-items: flex-start;
+         align-items: flex-start;
+     }
+
+     .mc-r .right-img {
+         -webkit-box-ordinal-group: 2;
+         -moz-box-ordinal-group: 2;
+         -ms-flex-order: 2;
+         -webkit-order: 2;
+         order: 2;
+     }
+
+     .mc-r .right-text {
+         -webkit-box-ordinal-group: 1;
+         -moz-box-ordinal-group: 1;
+         -ms-flex-order: 1;
+         -webkit-order: 1;
+         order: 1;
+     }
+ }
+ @media (max-width: 480px) {
+     .btn-pos {
+         margin-top: -20%;
+     }
+ }
+
+</style>
+
+
 <section class="section">
-  <div class="container">
-    <div class="row">
-    <div class="col-sm-5">
-    <br/> <br/>
-    <h4 class="mt-5">Tarif Layanan</h4>
-    <small>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet</small>
-    <br/> <br/>
-    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/' . $navbar['NONE'][2][$bahasa . '_slug'] . '/' .$bahasa)}}">Selengkapnya</a></p>
+    <div class="container mc">
+        <div class="row">
+            <div class="col-sm-6" id="navbar-upper-text">
+                <h4 class="mt-5">Tarif Layanan</h4>
+                <small>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet</small>
+            </div>
+            <div class="col-sm-5"><img id="img-navbar-temp" src="{{asset('asset/tarif-layanan.png')}}" alt="Logo" width="600px" style="left: 686px; height: 360px; margin-bottom: 8%;" class="img-responsive" /></div>
+        </div>
+        <div class="row btn-pos">
+            <div class="col-sm-12 mt-5">
+                <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/' . $navbar['NONE'][2][$bahasa . '_slug'] . '/' .$bahasa)}}">Selengkapnya</a></p>
+            </div>
+        </div>
     </div>
 
-    <div class="col-sm-5">
-    <img src="{{asset('asset/tarif-layanan.png')}}"  alt="Logo" width="600px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
+    <div class="container">
+        <div class="row mc-r">
+            <div class="col-sm-6 right-img" >
+                <img id="img-navbar-temp" src="{{asset('asset/bank-garansi.png')}}" alt="Logo" style="margin-left: -50px;" width="600px" class="img-responsive" />
+            </div>
+            <div class="col-sm-5 right-text" id="navbar-upper-text">
+                <h4 class="mt-5">Bank Garansi</h4>
+                <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung atau dijamin oleh pihak ketiga yang dapat dipercaya.</small>
+            </div>
+        </div>
+        <div class="row r-btn" style="">
+            <div class="col-sm-12">
+                <p><a  class="btn btn-sm btn-yellow-sm mt-4 btn-right"  role="button" href="{{url('article/' . $navbar['NONE'][3][$bahasa . '_slug'] . '/' .$bahasa)}}">Selengkapnya</a></p>
+            </div>
+        </div>
     </div>
+</br>
+</br>
+</br>
+    <div class="container mc">
+        <div class="row">
+            <div class="col-sm-6" id="navbar-upper-text">
+                <h4 class="mt-5">Referensi Bank</h4>
+                <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung oleh pihak ketiga yang dapat dipercaya.</small>
+            </div>
+            <div class="col-sm-5"><img id="img-navbar-temp" src="{{asset('asset/referensi-bank.png')}}" alt="Logo" width="600px" style="left: 686px; height: 360px; margin-bottom: 8%;" class="img-responsive" /></div>
+        </div>
+        <div class="row btn-pos">
+            <div class="col-sm-12 mt-5">
+                <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/' . $navbar['NONE'][4][$bahasa . '_slug'] . '/' .$bahasa)}}">Selengkapnya</a></p>
+            </div>
+        </div>
     </div>
-  </div>
-  <br/>
-
-  <div class="container">
-    <div class="row">
-    <div class="col-sm-7">
-    <img src="{{asset('asset/bank-garansi.png')}}"  alt="Logo" width="600px" style="height: 350px; margin-bottom:8%;">
-    </div>
-
-    <div class="col-sm-5">
-    <br/> <br/>
-    <h4 class="mt-5">Bank Garansi</h4>
-    <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung atau dijamin oleh pihak ketiga yang dapat dipercaya.</small>
-    <br/> <br/>
-    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/' . $navbar['NONE'][3][$bahasa . '_slug'] . '/' .$bahasa)}}">Berita Mantap</a></p>
-    </div>
-    </div>
-  </div>
-  <br/>
-
-  <div class="container">
-    <div class="row">
-    <div class="col-sm-5">
-    <br/> <br/>
-    <h4 class="mt-5">Referensi Bank</h4>
-    <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung oleh pihak ketiga yang dapat dipercaya.</small>
-    <br/> <br/>
-    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/' . $navbar['NONE'][4][$bahasa . '_slug'] . '/' .$bahasa)}}">Selengkapnya</a></p>
-    </div>
-
-    <div class="col-sm-5">
-    <img src="{{asset('asset/referensi-bank.png')}}"  alt="Logo" width="600px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
-    </div>
-    </div>
-  </div>
-  <br/>
 
   <div class="container">
-    <div class="row">
-    <div class="col-sm-7">
-    <img src="{{asset('asset/pic-transfer.png')}}"  alt="Logo" width="600px" style="height: 350px; margin-bottom:8%;">
+        <div class="row mc-r">
+            <div class="col-sm-6 right-img" >
+                <img id="img-navbar-temp" src="{{asset('asset/pic-transfer.png')}}" alt="Logo" style="margin-left: -50px;" width="600px" class="img-responsive" />
+            </div>
+            <div class="col-sm-5 right-text" id="navbar-upper-text">
+                <h4 class="mt-5">Transfer</h4>
+                <small>Jasa pengiriman uang yang dilaksanakan atas permintaan dan untuk kepentingan nasabah.</small>
+            </div>
+        </div>
+        <div class="row r-btn" style="">
+            <div class="col-sm-12">
+                <p><a  class="btn btn-sm btn-yellow-sm mt-4 btn-right"  role="button" href="{{url('article/' . $navbar['NONE'][5][$bahasa . '_slug'] . '/' .$bahasa)}}">Selengkapnya</a></p>
+            </div>
+        </div>
     </div>
-
-    <div class="col-sm-5">
-    <br/> <br/>
-    <h4 class="mt-5">Transfer</h4>
-    <small>Jasa pengiriman uang yang dilaksanakan atas permintaan dan untuk kepentingan nasabah.</small>
-    <br/> <br/>
-    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/' . $navbar['NONE'][5][$bahasa . '_slug'] . '/' .$bahasa)}}">Selengkapnya</a></p>
+</br>
+</br>
+</br>
+ <div class="container mc">
+        <div class="row">
+            <div class="col-sm-6" id="navbar-upper-text">
+                <h4 class="mt-5">Inkaso</h4>
+                <small>Layanan Bank Mantap dalam rangka penagihan warkat-warkat valuta rupiah yang Bank tertariknya berada diluar wilayah Kliring.</small>
+            </div>
+            <div class="col-sm-5"><img id="img-navbar-temp" src="{{asset('asset/pic-inkaso.png')}}" alt="Logo" width="600px" style="left: 686px; height: 360px; margin-bottom: 8%;" class="img-responsive" /></div>
+        </div>
+        <div class="row btn-pos">
+            <div class="col-sm-12 mt-5">
+                <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/inkaso/'.$bahasa)}}">Selengkapnya</a></p>
+            </div>
+        </div>
     </div>
-    </div>
-  </div>
-
  <br/>
-  <div class="container">
-    <div class="row">
-    <div class="col-sm-5">
-    <br/> <br/>
-    <h4 class="mt-5">Inkaso</h4>
-    <small>Layanan Bank Mantap dalam rangka penagihan warkat-warkat valuta rupiah yang Bank tertariknya berada diluar wilayah Kliring.</small>
-    <br/> <br/>
-    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="{{url('article/inkaso/'.$bahasa)}}">Selengkapnya</a></p>
-    </div>
-
-    <div class="col-sm-5">
-    <img src="{{asset('asset/pic-inkaso.png')}}"  alt="Logo" width="550px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
-    </div>
-    </div>
-  </div>
- 
- 
-
 
 </section>
 
