4806f343acbf656c31f06ff63d72ca7c950342b7
kamal <kamaludinakbar0@gmail.com>
2021-05-24 15:20:50 +0700

    update error tombol

--- resources/views/promosi-mantap.blade.php
+++ resources/views/promosi-mantap.blade.php
@@ -40,7 +40,7 @@
 @endif
 
     <!-- Content Start Here -->
-<div class="container @if(!@berita) text-center @endif" id="news">
+<div class="container @if(!$berita) text-center @endif" id="news">
     @if($berita)
         <img class="img-responsive" src="{{ url('storage/'. $berita->gambar)}}">
         <div class="row">
