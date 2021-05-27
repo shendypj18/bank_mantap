263cecb379436c82a52b5d19498c2306bbf80c82
kamal <kamaludinakbar0@gmail.com>
2021-05-27 09:32:42 +0700

    update template table

--- resources/views/template-table.blade.php
+++ resources/views/template-table.blade.php
@@ -36,22 +36,88 @@
          color: #0F2B5B;
 
      }
+
+     @media (min-width:1025px){
+        .btn-o-tbl{
+             visibility: hidden;
+         }
+     }
+
+     @media (max-width:1025px){
+         .btn-tbl{
+             visibility: hidden;
+         }
+         .btn-txt{
+            border-radius: 25px 0px 0px 25px;
+            width: 100%;
+            border: 1px solid #0F2B5B;
+
+         }
+         .btn-txt:hover{
+            width: 100%;
+            border-radius: 25px 0px 0px 25px;
+         }
+         .txt-pil{
+             float: left;
+             margin-left: 50px;
+         }
+         .btn-group.btn-o-tbl{
+             margin-top: -50px;
+             margin-bottom: 80px;
+             width: 100%;
+         }
+         .btn-tgl{
+             border: 2px solid #0F2B5B
+         }
+         .th-btn{
+             display: none;
+         }
+         .desc-txt{
+             width: 60%;
+             margin-left: 10px;
+
+         }
+         .thn-txt{
+             width: 40%;
+         }
+     }
     </style>
     <div class="container">
+       <div class="row">
+        <div class="col-12">
+            <div class="btn-group btn-o-tbl">
+                <button class="btn btn-sm btn-primary bg-white btn-txt " type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#121212; ">
+                   <div class="txt-pil">
+                    Pilih Tahun
+                   </div>
+                </button>
+                <button type="button" class="btn btn-sm btn-primary dropdown-toggle text-right btn-tgl" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 20%;">
+                </button>
+                <div class="dropdown-menu dropdown-menu-right">
+                    @foreach($tahun as $tt)
+                        <a href="{{url('article/'. $navbardata[$bahasa . '_slug'] .'/?tahun=' . $tt->tahun)}}" class="dropdown-item" type="button">{{$tt->tahun}}</a>
+                    @endforeach
+                </div>
+            </div>
+           </div>
+       </div>
         <table>
             <thead>
                 <tr>
-                    <th class="pl-4" style="border-radius: 16px 0px 0px 0px;" colspan="2">Tahun</th>
-                    <th>Diskripsi</th>
+                    <th class="pl-4 thn-txt" style="border-radius: 16px 0px 0px 0px;" colspan="2">Tahun</th>
+                    <th class="desc-txt">Diskripsi</th>
 
-                    <th style="border-radius: 0px 16px 0px 0px; width:12%;">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-primary dropdown-toggle text-right" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Pilih Tahun
+                    <th class="th-btn" style="border-radius: 0px 16px 0px 0px; width:12%;">
+                        <div class="btn-group btn-tbl">
+                            <button class="btn btn-sm btn-primary bg-white " type="button" style="color:#121212;">
+                               Pilih Tahun
+                            </button>
+                            <button type="button" class="btn btn-sm btn-primary dropdown-toggle text-right" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 20%;">
+
                             </button>
                             <div class="dropdown-menu dropdown-menu-right">
-                                @foreach($tahun as $tahun)
-                                    <a href="{{url('article/'. $navbardata[$bahasa . '_slug'] .'/?tahun=' . $tahun->tahun)}}" class="dropdown-item" type="button">{{$tahun->tahun}}</a>
+                                @foreach($tahun as $cc)
+                                    <a href="{{url('article/'. $navbardata[$bahasa . '_slug'] .'/?tahun=' . $cc->tahun)}}" class="dropdown-item" type="button">{{$cc->tahun}}</a>
                                 @endforeach
                             </div>
                         </div>
@@ -67,8 +133,8 @@
                         <td class="text-right" style="width:4%">{{$i}} <span>.  </span></td>
                         <td>@if($l->jenis_laporan == "7") {{$l->nama. ' '}} @endif {{$l->tahun}}</td>
                         <td>{{$l->deskripsi}}</td>
-                        @if($l->nama_file)
-                        <td><a href="{{url('storage/'. $l->nama_file)}}">lihat file</a> <img class="ml-2" src="{{ asset('asset/download.svg') }}"></td>
+                        @if($l->jenis_laporan != "7")
+                            <td><a href="{{url('storage/'. $l->nama_file)}}">lihat file</a> <img class="ml-2" src="{{ asset('asset/download.svg') }}"></td>
                         @endif
                     </tr>
                     @php
