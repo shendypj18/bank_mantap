10c8f7976392e23649b4cbaf11924fc64f485a42
kamal <kamaludinakbar0@gmail.com>
2021-05-29 22:31:03 +0700

    update sekilas perushaan

--- resources/views/sekilas-perusahaan.blade.php
+++ resources/views/sekilas-perusahaan.blade.php
@@ -1,1 +1,18 @@
+<style type="text/css" >
+ @media (min-width: 480px) and (max-width: 678px) {
+   .btn-unduh {
+       width:50%;
+       background-size: 0%;
+       margin-left: 20%;
+    }
+
+ }
+ @media(min-width: 679px) {
+     .btn-unduh {
+         margin-left: 15%;
+     }
+ }
+
+</style>
+
 <div class="mx-auto mt-5"><p><a class="btn btn-unduh"  href="{{url('storage/' .$profil_lengkap->nama_file)}}" target="_blank">{{__('admin.download_sekilas_perusahaan')}}</a></p></div>
