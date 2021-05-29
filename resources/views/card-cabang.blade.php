741a045c39418f207240e0041c1375a8d529fac7
shendypj18 <shendypj18@gmail.com>
2021-05-29 18:42:28 +0700

    update card

--- resources/views/card-cabang.blade.php
+++ resources/views/card-cabang.blade.php
@@ -1,3 +1,8 @@
+<style>
+    .card-text-1 {
+        margin: 10px 15px;
+    }
+</style>
 <div class="mt-5">
 <div class="row mb-5">
     @php $i = 1; @endphp
@@ -6,10 +11,12 @@
         <div class="card branch" onclick="gmap({{$c}})">
             <div class="card-body">
                 <br/>
-                <img src="{{ asset('asset/logo_mantap.png') }}" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+                <img class="card-text-1" src="{{ asset('asset/logo_mantap.png') }}" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+                <div class="card-text card-text-1">
                 <small>{{$c->alamat}}</small><br/>
                 <small>{{$c->telp}}</small></p>
-                <h3><img src="{{ asset('asset/icon/maps.png') }}"  alt="Maps"  style="width: 5%"> &nbsp;<a>Lihat Map</a></h3>
+                </div>
+                <h3 class="card-link card-text-1"><img src="{{ asset('asset/icon/maps.png') }}"  alt="Maps"  style="width: 5%"> &nbsp;<a>Lihat Map</a></h3>
             </div>
         </div>
     </div>
