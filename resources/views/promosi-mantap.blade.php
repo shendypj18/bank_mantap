c4bba488ba0de36b94ea36a8753c00e65ee9352e
kamal <kamaludinakbar0@gmail.com>
2021-06-05 14:43:12 +0700

    update afrizal

--- resources/views/promosi-mantap.blade.php
+++ resources/views/promosi-mantap.blade.php
@@ -77,12 +77,6 @@
             <div class="col-md-10 mt-5">
                 <h4 class="fz-berita text-justify" ><strong>{{$berita[$bahasa .'_judul']}}<strong></h4>
             </div>
-
-            <div class="col-md-2 mt-5 ml-5">
-                @if($berita)
-                    <p class="text-center"><a class="btn btn-sm btn-share" data-toggle="modal" data-target="#ModalShare" data-whatever="share" href="#" role="button"><span class="fa fa-share-alt"></span> {{ __('bisnis.bagikan_konten') }}</a></p>
-                @endif
-                 </div>
         </div>
         <div class="row justify-content-md-center text-justify">
             <div class="col-md-10" >
@@ -92,7 +86,7 @@
     @endif
     <div class="col-sm-8 mt-3" style="float:none;margin:auto;">
         @if($berita)
-            <p class="text-center"><a class="btn btn-sm btn-share" data-toggle="modal" data-target="#ModalShare" data-whatever="share" href="#" role="button"><span class="fa fa-share-alt"></span> Bagikan</a></p>
+            <p class="text-center"><a class="btn btn-sm btn-share" data-toggle="modal" data-target="#ModalShare" data-whatever="share" href="#" role="button"><span class="fa fa-share-alt"></span> {{ __('bisnis.bagikan_konten') }}</a></p>
         @endif
         <h3 class="text-center" >{{ __('admin.dapatkan_informasi_berita') }} <strong>Bank Mantap</strong></h3>
     </div>
