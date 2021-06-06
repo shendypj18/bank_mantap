78a9c6985ca637c07f575f083f2470128f002e77
kamal <kamaludinakbar0@gmail.com>
2021-06-06 12:23:31 +0700

    update style email

--- resources/views/email-keluhan.blade.php
+++ resources/views/email-keluhan.blade.php
@@ -1,5 +1,102 @@
+<!-- <style type="text/css">
+     table {
+	 width: 750px;
+	 border-collapse: collapse;
+	 margin:50px auto;
+	 }
+
+     /* Zebra striping */
+     tr:nth-of-type(odd) {
+	 background: #eee;
+	 }
+
+     th {
+	 background: #3498db;
+	 color: white;
+	 font-weight: bold;
+	 }
+
+     td, th {
+	 padding: 10px;
+	 border: 1px solid #ccc;
+	 text-align: left;
+	 font-size: 18px;
+	 }
+
+     /*
+     Max width before this PARTICULAR table gets nasty
+     This query will take effect for any screen smaller than 760px
+     and also iPads specifically.
+     */
+     @media
+     only screen and (max-width: 760px),
+     (min-device-width: 768px) and (max-device-width: 1024px)  {
+
+	 table {
+	 width: 100%;
+	 }
+
+	 /* Force table to not be like tables anymore */
+	 table, thead, tbody, th, td, tr {
+	 display: block;
+	 }
+
+	 /* Hide table headers (but not display: none;, for accessibility) */
+	 thead tr {
+	 position: absolute;
+	 top: -9999px;
+	 left: -9999px;
+	 }
+
+	 tr { border: 1px solid #ccc; }
+
+	 td {
+	 /* Behave  like a "row" */
+	 border: none;
+	 border-bottom: 1px solid #eee;
+	 position: relative;
+	 padding-left: 50%;
+	 }
+
+	 td:before {
+	 /* Now like a table header */
+	 position: absolute;
+	 /* Top/left values mimic padding */
+	 top: 6px;
+	 left: 6px;
+	 width: 45%;
+	 padding-right: 10px;
+	 white-space: nowrap;
+	 /* Label the data */
+	 content: attr(data-column);
+
+	 color: #000;
+	 font-weight: bold;
+	 }
+
+     }
+     </style> -->
 <p> Pesan keluhan oleh {{$nama}}</p>
-<p> nama : {{ $nama  }} </p>
-<p> email : {{ $email }} </p>
-<p> telpon : {{ $telp }} </p>
-<p> pesan : {{ $pesan }} </p>
+<p> Nama : {{ $nama  }} </p>
+<p> Email : {{ $email }} </p>
+<p> Telepon : {{ $telp }} </p>
+<p> Pesan : {{ $pesan }} </p>
+
+<!-- <table>
+     <thead>
+     <tr>
+     <th>Nama</th>
+     <th>Email</th>
+     <th>Telepon</th>
+     <th>Pesan</th>
+     </tr>
+     </thead>
+     <tbody>
+     <tr>
+     <td data-column="Nama">{{$nama}}</td>
+     <td data-column="Email">{{$email}}</td>
+     <td data-column="Telepon">{{$telp}}</td>
+     <td data-column="pesan">{{$pesan}}</td>
+     </tr>
+     </tbody>
+     </table> -->
--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,3 +1,82 @@
+<!-- <style type="text/css">
+     table {
+	 width: 750px;
+	 border-collapse: collapse;
+	 margin:50px auto;
+	 }
+
+     /* Zebra striping */
+     tr:nth-of-type(odd) {
+	 background: #eee;
+	 }
+
+     th {
+	 background: #3498db;
+	 color: white;
+	 font-weight: bold;
+	 }
+
+     td, th {
+	 padding: 10px;
+	 border: 1px solid #ccc;
+	 text-align: left;
+	 font-size: 18px;
+	 }
+
+     /*
+     Max width before this PARTICULAR table gets nasty
+     This query will take effect for any screen smaller than 760px
+     and also iPads specifically.
+     */
+     @media
+     only screen and (max-width: 760px),
+     (min-device-width: 768px) and (max-device-width: 1024px)  {
+
+	 table {
+	 width: 100%;
+	 }
+
+	 /* Force table to not be like tables anymore */
+	 table, thead, tbody, th, td, tr {
+	 display: block;
+	 }
+
+	 /* Hide table headers (but not display: none;, for accessibility) */
+	 thead tr {
+	 position: absolute;
+	 top: -9999px;
+	 left: -9999px;
+	 }
+
+	 tr { border: 1px solid #ccc; }
+
+	 td {
+	 /* Behave  like a "row" */
+	 border: none;
+	 border-bottom: 1px solid #eee;
+	 position: relative;
+	 padding-left: 50%;
+	 }
+
+	 td:before {
+	 /* Now like a table header */
+	 position: absolute;
+	 /* Top/left values mimic padding */
+	 top: 6px;
+	 left: 6px;
+	 width: 45%;
+	 padding-right: 10px;
+	 white-space: nowrap;
+	 /* Label the data */
+	 content: attr(data-column);
+
+	 color: #000;
+	 font-weight: bold;
+	 }
+
+     }
+     </style> -->
+
 <p>Laporan whistle blowing oleh {{$nama_pelapor}}</p>
 <p>Nama Pelapor :{{ $nama_pelapor}}</p>
 <p>nomer telepon :{{ $nomer_telepon }}</p>
@@ -9,3 +88,33 @@
 <p>lokasi kejadian :{{ $lokasi_kejadian}}</p>
 <p>kronologis kejadian :{{ $kronologis_kejadian}}</p>
 <p>nominal :{{ $nominal }}</p>
+<!-- <table>
+     <thead>
+     <tr>
+     <th>Nama Pelapor</th>
+     <th>Nomer Telepon</th>
+     <th>Email</th>
+     <th>Tindakan</th>
+     <th>Nama Terlapor</th>
+     <th>Jabatan Terlapor</th>
+     <th>Waktu Kejadian</th>
+     <th>Lokasi Kejadian</th>
+     <th>Kronologi Kejadian</th>
+     <th>Nominal</th>
+     </tr>
+     </thead>
+     <tbody>
+     <tr>
+     <td data-column="Nama Pelapor">{{$nama_pelapor}}</td>
+     <td data-column="Nomer Telepon">{{$nomer_telepon}}</td>
+     <td data-column="Email">{{$email}}</td>
+     <td data-column="Tindakan">{{$tindakan}}</td>
+     <td data-column="Nama Terlapor">{{$nama_terlapor}}</td>
+     <td data-column="Jabatan Terlapor">{{$jabatan_terlapor}}</td>
+     <td data-column="Waktu Kejadian">{{$waktu_kejadian}}</td>
+     <td data-column="Lokasi Kejadian">{{$lokasi_kejadian}}</td>
+     <td data-column="Kronologi Kejadian">{{$kronologis_kejadian}}</td>
+     <td data-column="Nominal"></td>
+     </tr>
+     </tbody>
+     </table> -->
