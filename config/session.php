7c73f3d6407bd10759c6abed3f09a2700a85036d
kamal <kamaludinakbar0@gmail.com>
2021-06-17 19:26:07 +0700

    update session

--- config/session.php
+++ config/session.php
@@ -33,7 +33,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 5),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
